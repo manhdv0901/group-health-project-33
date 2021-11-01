bceee51cfff283f5a6f79380ceb757ac0bf5b8e4
= <Manhdvph10986@fpt.edu.vn>
2021-11-01 17:26:11 +0700

    fixx error line 255

--- index.js
+++ index.js
@@ -292,8 +292,8 @@ app.post('/data-a-patient',(req, res)=>{
         if (err){
             res.status(404).json(err);
         }else {
-            const key_device = patient.key_device;
-            DEVICE.findOne({key_device: key_device})
+            const device_sensor = patient.key_device;
+            DEVICE.findOne({key_device: device_sensor})
                 .exec((err, device) =>{
                     if(err){
                         res.status(404).json(err);
