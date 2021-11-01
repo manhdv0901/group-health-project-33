8628d11c89e47b8bef8684e5a9bde8d17f97c26b
= <Manhdvph10986@fpt.edu.vn>
2021-11-01 14:32:15 +0700

    fix err line 295

--- index.js
+++ index.js
@@ -292,8 +292,8 @@ app.post('/data-a-patient',(req, res)=>{
         if (err){
             res.status(404).json(err);
         }else {
-            const keyDevice = patient.key_device;
-            DEVICE.findOne({key_device: keyDevice})
+            const key_kevice = patient.key_device;
+            DEVICE.findOne({key_device: key_kevice})
                 .exec((err, device) =>{
                     if(err){
                         res.status(404).json(err);
