64f9cfe3d2e37f7ae3e825a439e1c8f0e91c6895
nduc9 <ducnvph10517@fpt.edu.vn>
2021-11-14 22:35:52 +0700

    commit 14//11

--- notification.js
+++ notification.js
@@ -2,31 +2,56 @@ const express=require('express')
 const fetch=require('node-fetch');
 
 const  router=express.Router();
+const SERVER_KEY = 'AAAAqRousOQ:APA91bGX-6Wo0hGqvr9OrzCnX-8LEPNQXZsycdQR7qnrOH1Wzi5LDpo9UPyLNMayuL7F5QWXGI9wAxpRMwi7fOWRh3BrPHj9Nsc_5Fimt9Bb6wBO_GmbT97BDqXfZJNX4v2l_OXGAPsH';
 
 router.post('/sendToAll',(req,res)=>{
     var notification={
-        'title':'Hello World',
-        'text':'Subtitle'
+        'id':req.body.id,
+        'title':req.body.title,
+        'name':req.body.name,
+        'room':req.body.room,
+        'age':req.body.age,
+        'status':req.body.status,
+        'medicine':req.body.medicine,
+        'amountAndUse':req.body.amountAndUse
     };
-    var fcm_tokens=['dmV-fxmTSMaMex4Mu3Kgsp:APA91bE4VF0wrR8k27ZpEX6OrZO_tvYKq5A4A3swigc9dsKWtPWkwrzCAkfk78iw3AB7ci1zm7l_VjO4jdSWj2JBgJ0XT2AHsFBl5gSUnzDPbYcbKOA8fdWNfQJ5O6qhiQjDgjEmFslU'];
+    // var notification={
+    //     'title':'hello',
+    //     'text':'Subtitle'
+    // };
+    var fcm_tokens=req.body.token;//lấy từ body mà. m ảo thế
+    // var fcm_tokens=['ev8StG0CSHGv4RMdH9ndkZ:APA91bFu-TlfT-meH75l4h3CmxamvsVZrCERhvmUoBintP2cSTITYmAj7oXfWmEwTRGYCaMx3IHGjRGJRF0J-3zagt8b7O7tS37eEtkzIxLGRNeY_BrWdOBngYlmKb6sPvQKgpjQCNvR'];
 
     var notification_body={
-        'notification':notification,
-        'registration_ids':fcm_tokens
+        'data':notification,
+        'to':fcm_tokens
     }
-     fetch('https://fcm.googleapis.com/fcm/send',{
-         'method':'POST',
-         'headers':{
-             'Authorization':'key=' +
-                 'AAAATbEPNMA:APA91bH2zFbq58xlvGEUuaw6_sSqFWHkumKr5ZwkyTAu0l9EwtpydSYUbmI2SKy1eN7bybf2wsntMvq2Z4DSH1EfywE11caSjQyamqwNGuuv5ARAzkXuZSTUHimQMI-4jU9IHvPh7KRE',
-             'Content-Type':'application/json'
-         },
-         'body':JSON.stringify(notification_body)
-     }).then(()=>{
-         res.status(200).send('Notification succcesfully')
-     }).catch((err)=>{
-         res.status(400).send('Something went wrong!');
-         console.log(err)
-     })
+    //còn bắn node là bắn theo token, tại chiều bắn theo token k đ nên bắn console
+    // var notification_body={
+    //     'notification':notification,
+    //     'registration_ids':fcm_tokens
+    // } nch là api k liên quan đâu
+    console.log("notify body",notification_body)
+
+    async function fetchMovies() {
+        const response = await fetch('https://fcm.googleapis.com/fcm/send',{
+            'method':'POST',
+            'headers':{
+                'Authorization':'key=' +SERVER_KEY,
+                'Content-Type':'application/json'
+            },
+            'body':JSON.stringify(notification_body)
+        }).then(()=>{
+            res.status(200).send('Notification succcesfully')
+        }).catch((err)=>{
+            res.status(400).send('Something went wrong!');
+            console.log(err)
+        });
+        return response;
+        // waits until the request completes...
+    }
+
+    console.log(fetchMovies());
 });
+
 module.exports=router
