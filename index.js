252b5c96be73d113544c9496d94d2a9c57299fc5
nduc9 <ducnvph10517@fpt.edu.vn>
2021-11-19 23:42:46 +0700

    commit 19/11 demo

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app =express();
 const mongoose = require("mongoose");
 const path = require('path')
 const fetch = require("node-fetch");
+const SERVER_KEY = 'AAAAqRousOQ:APA91bGX-6Wo0hGqvr9OrzCnX-8LEPNQXZsycdQR7qnrOH1Wzi5LDpo9UPyLNMayuL7F5QWXGI9wAxpRMwi7fOWRh3BrPHj9Nsc_5Fimt9Bb6wBO_GmbT97BDqXfZJNX4v2l_OXGAPsH';
 app.use(expressSession({secret:'max',saveUninitialized:false,resave:false}));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({
@@ -117,64 +118,59 @@ app.get('/',(req, res)=>{
     res.render('login');
 })
 //send notification
-app.post('/sendToAll/:key',(req,res)=>{
-    console.log(req.params.key)
-    // var id=req.body.id;
-    // var title=req.body.title;
-    // var name=req.body.name;
-    // var room=req.body.room;
-    // var age=req.body.age;
-    // var status=req.body.status;
-    // var medicine=req.body.medicine;
-    // var amountAndUse=req.body.amountAndUse;
-    //
-    //     var notification={
-    //         'id':id,
-    //         'title':title,
-    //         'name':name,
-    //         'room':room,
-    //         'age':age,
-    //         'status':status,
-    //         'medicine':medicine,
-    //         'amountAndUse':amountAndUse,
-    //     };
-    //     // var notification={
-    //     //     'title':'hello',
-    //     //     'text':'Subtitle'
-    //     // };
-    //     var fcm_tokens=req.body.token;//lấy từ body mà. m ảo thế
-    //     // var fcm_tokens=['ev8StG0CSHGv4RMdH9ndkZ:APA91bFu-TlfT-meH75l4h3CmxamvsVZrCERhvmUoBintP2cSTITYmAj7oXfWmEwTRGYCaMx3IHGjRGJRF0J-3zagt8b7O7tS37eEtkzIxLGRNeY_BrWdOBngYlmKb6sPvQKgpjQCNvR'];
-    //
-    //     var notification_body={
-    //         'data':notification,
-    //         'to':fcm_tokens
-    //     }
-    //     //còn bắn node là bắn theo token, tại chiều bắn theo token k đ nên bắn console
-    //     // var notification_body={
-    //     //     'notification':notification,
-    //     //     'registration_ids':fcm_tokens
-    //     // } nch là api k liên quan đâu
-    //     console.log("notify body",notification_body)
-    //
-    //     async function fetchMovies() {
-    //         const response = await fetch('https://fcm.googleapis.com/fcm/send',{
-    //             'method':'POST',
-    //             'headers':{
-    //                 'Authorization':'key=' +SERVER_KEY,
-    //                 'Content-Type':'application/json'
-    //             },
-    //             'body':JSON.stringify(notification_body)
-    //         }).then(()=>{
-    //             res.status(200).send('Notification succcesfully')
-    //         }).catch((err)=>{
-    //             res.status(400).send('Something went wrong!');
-    //             console.log(err)
-    //         });
-    //         return response;
-    //         // waits until the request completes...
-    //     }
-    //
-    //     console.log(fetchMovies());
+app.post('/sendToAll',(req,res)=>{
+    var id=req.body.id;
+    var title=req.body.title;
+    var name=req.body.name;
+    var room=req.body.room;
+    var age=req.body.age;
+    var status=req.body.status;
+    var medicine=req.body.medicine;
+    var amountAndUse=req.body.amountAndUse;
+        var notification={
+            'id':id,
+            'title':title,
+            'name':name,
+            'room':room,
+            'age':age,
+            'status':status,
+            'medicine':medicine,
+            'amountAndUse':amountAndUse,
+        };
+
+        var fcm_tokens=req.body.token;//lấy từ body mà. m ảo thế
+        // var fcm_tokens=['ev8StG0CSHGv4RMdH9ndkZ:APA91bFu-TlfT-meH75l4h3CmxamvsVZrCERhvmUoBintP2cSTITYmAj7oXfWmEwTRGYCaMx3IHGjRGJRF0J-3zagt8b7O7tS37eEtkzIxLGRNeY_BrWdOBngYlmKb6sPvQKgpjQCNvR'];
+
+        var notification_body={
+            'data':notification,
+            'to':fcm_tokens
+        }
+        //còn bắn node là bắn theo token, tại chiều bắn theo token k đ nên bắn console
+        // var notification_body={
+        //     'notification':notification,
+        //     'registration_ids':fcm_tokens
+        // } nch là api k liên quan đâu
+        console.log("notify body",notification_body)
+
+        async function fetchMovies() {
+            const response = await fetch('https://fcm.googleapis.com/fcm/send',{
+                'method':'POST',
+                'headers':{
+                    'Authorization':'key=' +SERVER_KEY,
+                    'Content-Type':'application/json'
+                },
+                'body':JSON.stringify(notification_body)
+            }).then(()=>{
+                res.status(200).send('Notification succcesfully')
+            }).catch((err)=>{
+                res.status(400).send('Something went wrong!');
+                console.log(err)
+            });
+            return response;
+            // waits until the request completes...
+        }
+
+        console.log(fetchMovies());
 },
 );
 app.post("/add-device", (req,res) => {
